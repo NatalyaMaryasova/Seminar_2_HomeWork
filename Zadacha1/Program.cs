5b6d8224d0f33713ceb44bdc39ab52a9942e3664
Natalya <nanona@example.com>
2022-11-17 14:30:06 +0700

    Added Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа*/
+
+int randomNumder = new Random().Next(100, 1000);
+int firstDigit = randomNumder / 10;
+int secondDigit = firstDigit % 10;
+
+Console.WriteLine(randomNumder + "->" + secondDigit);
